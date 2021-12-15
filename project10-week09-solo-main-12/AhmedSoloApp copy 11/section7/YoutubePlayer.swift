ce83fb40d76095b27a452bc6795b1ba2f16f05ea
ahmed <a707a7med@gmail.com>
2021-12-15 09:13:32 +0300

    add page youtube

--- project10-week09-solo-main-12/AhmedSoloApp copy 11/section7/YoutubePlayer.swift
+++ project10-week09-solo-main-12/AhmedSoloApp copy 11/section7/YoutubePlayer.swift
@@ -13,9 +13,9 @@ import SideMenu
 class Youtube0 : UIViewController , YTPlayerViewDelegate {
     
     @IBOutlet weak var U1: YTPlayerView!
-    @IBOutlet weak var U2: YTPlayerView!
+   // @IBOutlet weak var U2: YTPlayerView!
     @IBOutlet weak var U3: YTPlayerView!
-    @IBOutlet weak var U4: YTPlayerView!
+   // @IBOutlet weak var U4: YTPlayerView!
     @IBOutlet weak var U5: YTPlayerView!
     @IBOutlet weak var U6: YTPlayerView!
     
@@ -23,23 +23,23 @@ class Youtube0 : UIViewController , YTPlayerViewDelegate {
         super.viewDidLoad()
         
         
-        U1.load(withVideoId: "1tT-IlDd1bE", playerVars: ["playsinline": "1"])
+        U1.load(withVideoId: "BvEVRuIo_kA", playerVars: ["playsinline": "1"])
         U1.delegate = self
         
-        U2.load(withVideoId: "g1WJI9O6fyA", playerVars: ["playsinline": "1"])
-        U2.delegate = self
+//        U2.load(withVideoId: "BvEVRuIo_kA", playerVars: ["playsinline": "1"])
+//        U2.delegate = self
         
-        U3.load(withVideoId: "g1WJI9O6fyA", playerVars: ["playsinline": "1"])
+        U3.load(withVideoId: "gUC3TjCrwRw", playerVars: ["playsinline": "1"])
         U3.delegate = self
         
-        U4.load(withVideoId: "g1WJI9O6fyA", playerVars: ["playsinline": "1"])
-        U4.delegate = self
+//        U4.load(withVideoId: "gUC3TjCrwRw", playerVars: ["playsinline": "1"])
+//        U4.delegate = self
         
-        U5.load(withVideoId: "g1WJI9O6fyA", playerVars: ["playsinline": "1"])
+        U5.load(withVideoId: "GkVCrN2Yk0Q", playerVars: ["playsinline": "1"])
         U5.delegate = self
         
-        U6.load(withVideoId: "g1WJI9O6fyA", playerVars: ["playsinline": "1"])
-        U6.delegate = self
+//        U6.load(withVideoId: "g1WJI9O6fyA", playerVars: ["playsinline": "1"])
+//        U6.delegate = self
         
         
         
